073af5749308e89c3e263bd280e1ad9981e16d67
Diksha Singh <dikshasingh25202520@gmail.com>
2025-03-29 19:22:23 +0530

    add project files

--- Routes/listing.js
+++ Routes/listing.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const wrapAsync = require("../utils/wrapAsync.js");
+const wrapAsync = require("../utils/WrapAsync.js");
 const { listingSchema } = require("../schema.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
